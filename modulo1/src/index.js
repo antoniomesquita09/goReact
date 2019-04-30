77d12bea3a3137a878d9e46861f2c38e3ca9340a
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-04-29 21:43:28 -0300

    modulo 1

--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -1,29 +1,29 @@
-import React, { Component, Fragment } from "React";
-import { render } from "react-dom";
+import React, { Component, Fragment } from 'React'
+import { render } from 'react-dom'
 
-import "./style.scss";
-import Button from "./Button";
+import './style.scss'
+import Button from './Button'
 
 class App extends Component {
   state = {
     counter: 0
-  };
+  }
 
-  componentDidMount() {}
+  componentDidMount () {}
 
   handleClick = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+    this.setState({ counter: this.state.counter + 1 })
+  }
 
-  render() {
+  render () {
     return (
       <Fragment>
         <h1 className="title">Hello World</h1>
-        <h2 style={{ color: "#f00" }}>{this.state.counter}</h2>
+        <h2 style={{ color: '#f00' }}>{this.state.counter}</h2>
         <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
-    );
+    )
   }
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'))
